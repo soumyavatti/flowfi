526434310994342bbaa374f7eaee1263cb422fae
Soumya <101737464+soumyavatti@users.noreply.github.com>
2025-11-12 13:24:27 -0500

    Add files via upload

---
+++ channels.ts
@@ -0,0 +1,667 @@
+import type { ChannelInfo } from "@/types"
+
+export const channelsList: ChannelInfo[] = [
+  {
+    id: "r3JG5gBLbpA",
+    title: "Lost in Japan",
+    icon: "https://img.icons8.com/material-outlined/24/city.png",
+  },
+  {
+    id: "vrB9wC6quaU",
+    title: "Lofi on a Calm Night",
+    icon: "https://img.icons8.com/ios/50/balcony.png",
+  },
+  {
+    id: "92PvEVG0sKI",
+    title: "Cozy Lofi",
+    icon: "https://img.icons8.com/ios-glyphs/30/city-buildings.png",
+  },
+  {
+    id: "vYIYIVmOo3Q",
+    title: "Rainy Rooftop",
+    icon: "https://img.icons8.com/ios-glyphs/30/rain--v1.png",
+  },
+  {
+    id: "yf5NOyy1SXU",
+    title: "Room with City View",
+    icon: "https://img.icons8.com/ios-filled/50/empty-bed.png",
+  },
+  {
+    id: "IOOXppTp5co",
+    title: "Lock in with Bear",
+    icon: "https://img.icons8.com/ios-filled/50/bear-footprint.png",
+  },
+  {
+    id: "Fs-RjtIDvbw",
+    title: "Coffee Shop",
+    icon: "https://img.icons8.com/material-rounded/24/espresso-cup--v1.png",
+  },
+  {
+    id: "tVHNkTBvCtI",
+    title: "Hip Hop Lofi",
+    icon: "https://img.icons8.com/ios/50/rap.png",
+  },
+  {
+    id: "mF3m7Jza2uc",
+    title: "Video Game Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/nintendo-switch-pro-controller.png",
+  },
+  {
+    id: "JQtM2tyWAOQ",
+    title: "Chinese Vibes",
+    icon: "https://img.icons8.com/external-jumpicon-glyph-ayub-irawan/32/external-_39-chinese-new-year-jumpicon-(glyph)-jumpicon-glyph-ayub-irawan.png",
+  },
+  {
+    id: "rPjez8z61rI",
+    title: "Chill with Pig",
+    icon: "https://img.icons8.com/ios-filled/50/tv.png",
+  },
+  {
+    id: "techmgGVOhk",
+    title: "Frog on a Rainy Night",
+    icon: "https://img.icons8.com/ios-filled/50/frog.png",
+  },
+  {
+    id: "UedTcufyrHc",
+    title: "Synthwave Lofi",
+    icon: "https://img.icons8.com/external-glyph-andi-nur-abdillah/64/external-Floor-outline-(glyph)-glyph-andi-nur-abdillah.png",
+  },
+  {
+    id: "IBKIzCxy55o",
+    title: "Chill with Spongebob",
+    icon: "https://img.icons8.com/material-rounded/24/sofa.png",
+  },
+  {
+    id: "Rm_AsflD2c0",
+    title: "Drive with Spongebob",
+    icon: "https://img.icons8.com/ios-filled/50/steering-wheel.png",
+  },
+  {
+    id: "VJY9ZdHnNr0",
+    title: "Reflect with Spongebob",
+    icon: "https://img.icons8.com/ios-filled/50/moon.png",
+  },
+  {
+    id: "bGA1cU_qWMQ",
+    title: "Sleep with Spongebob",
+    icon: "https://img.icons8.com/external-wanicon-lineal-wanicon/64/external-comforter-winter-wanicon-lineal-wanicon.png",
+  },
+  {
+    id: "sqPJMvgbxQw",
+    title: "Beats with BMO",
+    icon: "https://img.icons8.com/external-others-nixx-design/67/external-retro-game-lineal-others-nixx-design-2.png",
+  },
+  {
+    id: "5P6fObPCAmU",
+    title: "Study with Bart Simpson",
+    icon: "https://img.icons8.com/ios/50/bart-simpson.png",
+  },
+  {
+    id: "m0hD2iFaSW4",
+    title: "Lord of the Rings Lofi",
+    icon: "https://img.icons8.com/ios/50/gandalf.png",
+  },
+  {
+    id: "Mfq3_nvFiFw",
+    title: "City View in Room",
+    icon: "https://img.icons8.com/external-outline-juicy-fish/60/external-student-school-outline-outline-juicy-fish.png",
+  },
+  {
+    id: "UqS3zt4crtM",
+    title: "Relax with Bluey",
+    icon: "https://img.icons8.com/ios-filled/50/bluey.png",
+  },
+  {
+    id: "7B1cml6AZ14",
+    title: "Relax with Bluey 2",
+    icon: "https://img.icons8.com/ios-filled/50/umbrella.png",
+  },
+  {
+    id: "QXuaIH_gWyw",
+    title: "Chill with R&B",
+    icon: "https://img.icons8.com/external-kmg-design-detailed-outline-kmg-design/64/external-curtain-furniture-kmg-design-detailed-outline-kmg-design.png",
+  },
+  {
+    id: "IjUq6lDLLc4",
+    title: "Upbeat Lofi",
+    icon: "https://img.icons8.com/material/24/earrings.png",
+  },
+  {
+    id: "9IOmDeoHSo8",
+    title: "Fantasy Lofi",
+    icon: "https://img.icons8.com/ios-glyphs/30/fantasy--v1.png",
+  },
+  {
+    id: "so9hIGAWwIs",
+    title: "Meditation Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/meditation-guru.png",
+  },
+  {
+    id: "DANGDUDaJd0",
+    title: "Cozy Lofi Girl",
+    icon: "https://img.icons8.com/external-nawicon-detailed-outline-nawicon/64/external-pajamas-bedroom-nawicon-detailed-outline-nawicon.png",
+  },
+  {
+    id: "GOhnPPjgjfI",
+    title: "Cozy Lofi Girl 2",
+    icon: "https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/external-sweater-winter-vitaliy-gorbachev-lineal-vitaly-gorbachev.png",
+  },
+  {
+    id: "p6_D5I4IxdA",
+    title: "Gospel Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/guitar.png",
+  },
+  {
+    id: "RrWBMwOsdkI",
+    title: "Draw with Bart Simpson",
+    icon: "https://img.icons8.com/ios-filled/50/wacom-tablet.png",
+  },
+  {
+    id: "fUB8AeKwwHI",
+    title: "Lock In with Bart Simpson",
+    icon: "https://img.icons8.com/ios-filled/50/headphones.png",
+  },
+  {
+    id: "yYTBBSUqyXI",
+    title: "Reflect at Motel with Bart",
+    icon: "https://img.icons8.com/external-others-pike-picture/50/external-Advertising-motel-others-pike-picture.png",
+  },
+  {
+    id: "CTAvv6Tpn_g",
+    title: "Smoke with Bart Simpson",
+    icon: "https://img.icons8.com/quill/100/joint.png",
+  },
+  {
+    id: "-YY89olDSpo",
+    title: "Chill at the Station",
+    icon: "https://img.icons8.com/dotty/80/subway.png",
+  },
+  {
+    id: "qE3Sza3KDN4",
+    title: "Boba with Lofi Girl",
+    icon: "https://img.icons8.com/ios-filled/50/bubble-tea-.png",
+  },
+  {
+    id: "YBrbLXpXZdA",
+    title: "Lock in with Lofi Girl",
+    icon: "https://img.icons8.com/ios/50/book-and-pencil.png",
+  },
+  {
+    id: "9nf0dvTP8yY",
+    title: "Study with Lofi Girl 2",
+    icon: "https://img.icons8.com/ios-filled/50/lights.png",
+  },
+  {
+    id: "7XrwdXTy-p8",
+    title: "Adventure Time",
+    icon: "https://img.icons8.com/ios/50/jake.png",
+  },
+  {
+    id: "p_oz1qIdJlI",
+    title: "Rainy Day with Dog",
+    icon: "https://img.icons8.com/ios-filled/50/rain--v1.png",
+  },
+  {
+    id: "dk6fPqa-uZQ",
+    title: "Christian Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/holy-bible.png",
+  },
+  {
+    id: "j5TAqVqZF6A",
+    title: "Pokemon Gen 1-7",
+    icon: "https://img.icons8.com/glyph-neue/64/koi-fish.png"
+  },
+  {
+    id: "5yx6BWlEVcY",
+    title: "Raccoon on a Walk",
+    icon: "https://img.icons8.com/ios-filled/50/chillhop-music.png"
+  },
+  {
+    id: "0pb3E4PGxq8",
+    title: "Cherry Blossom",
+    icon: "https://img.icons8.com/ios-glyphs/30/sakura.png"
+  },
+  {
+    id: "T8Cq3AXBEpM",
+    title: "R&B Lofi",
+    icon: "https://img.icons8.com/ios-glyphs/30/afro-pick.png",
+  },
+  {
+    id: "Fxq5vkdGmKE",
+    title: "Morning in Forest",
+    icon: "https://img.icons8.com/ios-filled/50/forest.png",
+  },
+  {
+    id: 'erUTqlcsDJI',
+    title: 'Wake up with Lofi',
+    icon: 'https://img.icons8.com/external-photo3ideastudio-solid-photo3ideastudio/64/external-digital-clock-sleep-photo3ideastudio-solid-photo3ideastudio.png'
+  },
+  {
+    id: 'hB2LatX6NLg',
+    title: 'Shibuya Nights',
+    icon: 'https://img.icons8.com/ios-filled/50/japan.png'
+  },
+  {
+    id: 'CX9_h23icoM',
+    title: 'Radio Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/tabletop-radio.png'
+  },
+  {
+    id: 'htuPmMDP-BY',
+    title: 'Interstellar Lofi',
+    icon: 'https://img.icons8.com/dotty/80/universe.png'
+  },
+  {
+    id: '4LIu4EyuDXI',
+    title: 'Pokemon Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/egg-pokemon.png'
+  },
+  {
+    id: 'n90fDtZjlJg',
+    title: 'Chill VHS Radio',
+    icon: 'https://img.icons8.com/ios-filled/50/tape-drive.png'
+  },
+  {
+    id: 'Vg13S-zzol0',
+    title: 'Raining in Japan',
+    icon: 'https://img.icons8.com/ios-filled/50/pagoda.png'
+  },
+  {
+    id: 'FYJ7RCDgFwE',
+    title: 'Late Night Cafe',
+    icon: 'https://img.icons8.com/ios-filled/50/cafe.png'
+  },
+  {
+    id: 'LC0fYEpy2ng',
+    title: 'Lift Your Mood',
+    icon: 'https://img.icons8.com/ios-filled/50/happy--v1.png'
+  },
+  {
+    id: 'nP-aOc7g228',
+    title: 'Garden Cafe',
+    icon: 'https://img.icons8.com/ios-filled/50/garden.png'
+  },
+  {
+    id: 'JmcvGrPt4sM',
+    title: 'Code with Bart Simpson',
+    icon: 'https://img.icons8.com/ios-filled/50/developer.png'
+  },
+  {
+    id: '5D5b0-hfzno',
+    title: 'Star Wars',
+    icon: 'https://img.icons8.com/ios-filled/50/star-wars.png'
+  },
+  {
+    id: 'jEIFHt4q6nA',
+    title: 'R&B and Neo Soul',
+    icon: 'https://img.icons8.com/ios-filled/50/saxophone.png'
+  },
+  {
+    id: 'IDZHHQsmvmc',
+    title: 'Beach View',
+    icon: 'https://img.icons8.com/ios-filled/50/beach.png'
+  },
+  {
+    id: 'yr9ZxQaWkqs',
+    title: 'Ramen Cafe',
+    icon: 'https://img.icons8.com/ios-filled/50/noodles.png'
+  },
+  {
+    id: 'eHWSxR7ORxQ',
+    title: 'Ramen Cafe Raining',
+    icon: 'https://img.icons8.com/carbon-copy/100/noodles.png'
+  },
+  {
+    id: 'veqc3aP7WYs',
+    title: 'Raining at a Bookstore',
+    icon: 'https://img.icons8.com/external-yogi-aprelliyanto-glyph-yogi-aprelliyanto/32/external-bookstore-book-store-yogi-aprelliyanto-glyph-yogi-aprelliyanto.png'
+  },
+  {
+    id: '_mHmfLfx0NU',
+    title: 'Trap-soul Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/cd--v1.png'
+  },
+  {
+    id: 'x0OLPEjna8A',
+    title: 'Chill at the Cafe',
+    icon: 'https://img.icons8.com/ios-filled/50/espresso-cup.png'
+  },
+  {
+    id: 'KMXZF-K2mus',
+    title: 'Code with Me',
+    icon: 'https://img.icons8.com/ios-filled/50/under-computer.png'
+  },
+  {
+    id: 'GB7kh1tvZxY',
+    title: 'Coffee Shop Lofi',
+    icon: 'https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/60/external-plates-kitchen-vitaliy-gorbachev-lineal-vitaly-gorbachev.png'
+  },
+  {
+    id: 'orFvdB0gJng',
+    title: 'Vintage Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/alto-saxophone.png'
+  },
+  {
+    id: 'd_t5nnK9Rn4',
+    title: 'Chill at the Lake',
+    icon: 'https://img.icons8.com/ios-filled/50/lotus.png'
+  },
+  {
+    id: '28KRPhVzCus',
+    title: 'Lofi to Sleep To',
+    icon: 'https://img.icons8.com/ios-filled/50/pillow.png'
+  },
+  {
+    id: 'ft6R-UNXLSs',
+    title: 'Cat on the Train',
+    icon: 'https://img.icons8.com/ios-filled/50/pet-commands-train.png'
+  },
+  {
+    id: 'C_9FdFnE5hY',
+    title: 'Sonic the Hedgehog',
+    icon: 'https://img.icons8.com/ios-filled/50/sonic-the-hedgehog-1.png'
+  },
+  {
+    id: 'dw_Bx0e0lis',
+    title: 'Just Another Cafe Shop',
+    icon: 'https://img.icons8.com/ios-filled/50/food-bar.png'
+  },
+  {
+    id: 'pxWfp8Hxazo',
+    title: 'Rainy Bookstore',
+    icon: 'https://img.icons8.com/ios-filled/50/book-shelf.png'
+  },
+  {
+    id: 'SabebXcovd9',
+    title: 'Enjoy Tea with Cat',
+    icon: 'https://img.icons8.com/fluency-systems-filled/48/green-tea.png'
+  },
+  {
+    id: 'MAlHxI0hc3A',
+    title: 'Chill in Futuristic Rooftop',
+    icon: 'https://img.icons8.com/ios-glyphs/30/hammock.png'
+  },
+  {
+    id: 'uFlzUaisbig',
+    title: 'Write with Lofi Girl',
+    icon: 'https://img.icons8.com/ios/50/autograph.png'
+  },
+  {
+    id: 'WSlv8uPoGhY',
+    title: 'Tea by the Windowsill',
+    icon: 'https://img.icons8.com/external-goofy-solid-kerismaker/96/external-Matcha-beverage-goofy-solid-kerismaker.png'
+  },
+  {
+    id: 'x9MolslcpzU',
+    title: 'Cyberpunk Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/vaporwave.png'
+  },
+  {
+    id: '1AOMHYL_pz4',
+    title: 'Lofi Girl Locked In',
+    icon: 'https://img.icons8.com/ios-filled/50/reading.png'
+  },
+  {
+    id: 'jfKfPfyJRdk',
+    title: 'OG Lofi Girl',
+    icon: 'https://img.icons8.com/ios-filled/50/hand-with-pen.png'
+  },
+  {
+    id: 'R4jvhTed_gM',
+    title: 'Lofi in The Living Room',
+    icon: 'https://img.icons8.com/ios-filled/50/portable-speaker2.png'
+  },
+  {
+    id: 'WcI5cDt37ec',
+    title: 'Office by The City',
+    icon: 'https://img.icons8.com/ios-filled/50/my-computer--v2.png'
+  },
+  {
+    id: 'iWjQKl4VEio',
+    title: 'Serene and Dreamy Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/lake.png'
+  },
+  {
+    id: '1FzJsKY1IeE',
+    title: 'Retro and Nostalgic Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/city-bench.png'
+  },
+  {
+    id: 't_P4mOSv5hY',
+    title: 'Positive Lofi',
+    icon: 'https://img.icons8.com/ios/50/cloud--v1.png'
+  },
+  {
+    id: '7NOSDKb0HlU',
+    title: 'Raccoon Studying',
+    icon: 'https://img.icons8.com/ios-filled/50/person-laying-down.png'
+  },
+  {
+    id: 'PRgS7hBgR1k',
+    title: 'Midnight by a Lake',
+    icon: 'https://img.icons8.com/ios-filled/50/bright-moon.png'
+  },
+  {
+    id: 'Lv0SzPoZltY',
+    title: 'Futuristic Room Lofi',
+    icon: 'https://img.icons8.com/ios-filled/50/workstation.png'
+  },
+  {
+    id: '6tWWPn1lYgU',
+    title: 'Cat by the Candlelight',
+    icon: 'https://img.icons8.com/ios-filled/50/latern.png'
+  },
+  {
+    id: "Na0w3Mz46GA",
+    title: "Asian Inspired Beats",
+    icon: "https://img.icons8.com/ios-filled/50/globe-asia.png"
+  },
+  {
+    id: "wJSg1H8wOUg",
+    title: "Soul Jazz Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/listening-to-music-on-headphones.png"
+  },
+  {
+    id: "pJE-euPdroo",
+    title: "Locked In College Girl",
+    icon: "https://img.icons8.com/ios-filled/50/glasses.png"
+  },
+  {
+    id: "dxyaNvclgE8",
+    title: "Purple Cat by The Window",
+    icon: "https://img.icons8.com/ios-filled/50/galaxy.png"
+  },
+  {
+    id: "WelpRyoV0UY",
+    title: "Shiba and Friend",
+    icon: "https://img.icons8.com/ios-filled/50/hachiko.png"
+  },
+  {
+    id: "qH3fETPsqXU",
+    title: "Sleep with Doggo",
+    icon: "https://img.icons8.com/ios-filled/50/sleep.png"
+  },
+  {
+    id: "IC38LWnquWw",
+    title: "Slow Quiet Night with Bart",
+    icon: "https://img.icons8.com/ios-filled/50/sun-glasses.png"
+  },
+  {
+    id: "kmFk56siyLk",
+    title: "View Skyline with Bart",
+    icon: "https://img.icons8.com/ios/50/skyscrapers.png"
+  },
+  {
+    id: "YBcVSZnwrME",
+    title: "Observe the City with Spiderman",
+    icon: "https://img.icons8.com/external-those-icons-lineal-those-icons/24/external-Spiderman-people-and-avatars-those-icons-lineal-those-icons.png"
+  },
+  {
+    id: "Pit-ZFCynXk",
+    title: "Ocean Cafe",
+    icon: "https://img.icons8.com/external-vitaliy-gorbachev-fill-vitaly-gorbachev/60/external-stool-furniture-vitaliy-gorbachev-fill-vitaly-gorbachev-1.png"
+  },
+  {
+    id: "P6Segk8cr-c",
+    title: "In Your Feels Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/sad.png"
+  },
+  {
+    id: "6Jsnem7i848",
+    title: "Evening at The Vinyl Store",
+    icon: "https://img.icons8.com/external-itim2101-fill-itim2101/64/external-music-store-store-itim2101-fill-itim2101-2.png"
+  },
+  {
+    id: "A_hmrykwR7g",
+    title: "Evening Smoke",
+    icon: "https://img.icons8.com/ios-filled/50/joint.png"
+  },
+  {
+    id: "IxPANmjPaek",
+    title: "Medieval Lofi",
+    icon: "https://img.icons8.com/ios-glyphs/30/medieval-crown.png"
+  },
+  {
+    id: "Lcdi9O2XB4E",
+    title: "Tokyo Drive",
+    icon: "https://img.icons8.com/ios-filled/50/car.png"
+  },
+  {
+    id: "YQZY4SX6fhY",
+    title: "Christian Lofi to Sleep To",
+    icon: "https://img.icons8.com/ios/50/cross.png"
+  },
+  {
+    id: "HuFYqnbVbzY",
+    title: "Jazz Lofi Radio with Lofi Girl",
+    icon: "https://img.icons8.com/ios-filled/50/jazz.png"
+  },
+  {
+    id: "0oJ3HD6E1Y0",
+    title: "Drive with Bart Simpson",
+    icon: "https://img.icons8.com/quill/100/road.png"
+  },
+  {
+    id: "2Y4PKTECpy8",
+    title: "Catholic Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/church.png"
+  },
+  {
+    id: "ZZ_l1OH6JdE",
+    title: "Smoke with Bart Simpson",
+    icon: "https://img.icons8.com/external-vitaliy-gorbachev-fill-vitaly-gorbachev/60/external-smoke-pipe-bad-habits-vitaliy-gorbachev-fill-vitaly-gorbachev.png"
+  },
+  {
+    id: "qXPoj_VYb3U",
+    title: "Bible Worship Lofi",
+    icon: "https://img.icons8.com/external-justicon-lineal-justicon/64/external-jesus-easter-day-justicon-lineal-justicon.png"
+  },
+  {
+    id: "fe9Q3vt3fKE",
+    title: "The City that Never Sleeps",
+    icon: "https://img.icons8.com/glyph-neue/64/new-york.png"
+  },
+  {
+    id: "ansI4JK4zxo",
+    title: "Futuristic Living Room",
+    icon: "https://img.icons8.com/ios-glyphs/30/sofa.png"
+  },
+  {
+    id: "If_UO5D9SdU",
+    title: "Peaceful Greenhouse",
+    icon: "https://img.icons8.com/wired/64/greenhouse.png"
+  },
+  {
+    id: "cbrDQniNsIQ",
+    title: "Rainy Office View",
+    icon: "https://img.icons8.com/ink/48/office.png"
+  },
+  {
+    id: "0Xc_w6XLZi0",
+    title: "Shooting Stars",
+    icon: "https://img.icons8.com/external-wanicon-lineal-wanicon/64/external-shooting-star-space-wanicon-lineal-wanicon.png"
+  },
+  {
+    id: "qoBg9MftQD0",
+    title: "Sci Fi Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/astronaut-helmet.png"
+  },
+  {
+    id: "GluJS2IAe_s",
+    title: "Video Game Lofi 2",
+    icon: "https://img.icons8.com/ios-glyphs/30/controller.png"
+  },
+  {
+    id: "R8cN_BmBSQg",
+    title: "Lofi by The Candlelight",
+    icon: "https://img.icons8.com/ios-filled/50/christmas-candle--v1.png"
+  },
+  {
+    id: "3lKxseLU8bI",
+    title: "Anime Girl",
+    icon: "https://img.icons8.com/ios-filled/50/manga.png"
+  },
+  {
+    id: "ObDXOXBGhYU",
+    title: "Christian Freestyle Lofi",
+    icon: "https://img.icons8.com/ios-filled/50/micro.png",
+  },
+  {
+    id: "MCAK0oFsvdQ",
+    title: "Beats to Pray To",
+    icon: "https://img.icons8.com/glyph-neue/64/pray.png",
+  },
+  {
+    id: "73QvE-_JZdg",
+    title: "Sleep with Goku",
+    icon: "https://img.icons8.com/hatch/64/anime.png",
+  },
+  {
+    id: 'i4rRlNTQHjA',
+    title: 'Coffee Corner',
+    icon: 'https://img.icons8.com/ios-filled/50/coffee-maker.png'
+  },
+  {
+    id: 'E77FyiOZMdg',
+    title: 'Rain on a Peaceful River',
+    icon: 'https://img.icons8.com/ios-filled/50/valley.png'
+  },
+  
+]
+
+
+// Add favorites functionality
+export type FavoriteChannel = {
+  id: string
+}
+
+export const getFavoriteChannels = (): FavoriteChannel[] => {
+  if (typeof window === "undefined") return []
+
+  const favorites = localStorage.getItem("lofiAppFavorites")
+  return favorites ? JSON.parse(favorites) : []
+}
+
+export const saveFavoriteChannels = (favorites: FavoriteChannel[]) => {
+  if (typeof window === "undefined") return
+
+  localStorage.setItem("lofiAppFavorites", JSON.stringify(favorites))
+}
+
+export const toggleFavorite = (channelId: string) => {
+  const favorites = getFavoriteChannels()
+  const isFavorite = favorites.some((fav) => fav.id === channelId)
+
+  if (isFavorite) {
+    saveFavoriteChannels(favorites.filter((fav) => fav.id !== channelId))
+  } else {
+    saveFavoriteChannels([...favorites, { id: channelId }])
+  }
+
+  return !isFavorite
+}
+
+export const isChannelFavorite = (channelId: string): boolean => {
+  const favorites = getFavoriteChannels()
+  return favorites.some((fav) => fav.id === channelId)
+}
